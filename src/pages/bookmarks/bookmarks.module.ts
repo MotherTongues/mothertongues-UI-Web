0c02944eb6c03fc635d99610247157349d9a7c24
roedoejet <aidanpine@shaw.ca>
2019-07-14 13:25:33 -0700

    added default bookmarks message for empty bookmarks

--- src/pages/bookmarks/bookmarks.module.ts
+++ src/pages/bookmarks/bookmarks.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material';
 
 import { Bookmarks } from './bookmarks';
 import { EntryListModule } from '../shared/entry-list.module';
@@ -9,6 +10,7 @@ import { EntryListModule } from '../shared/entry-list.module';
 @NgModule({
   imports: [
     CommonModule,
+    MatIconModule,
     FormsModule,
     EntryListModule,
     RouterModule.forChild([
